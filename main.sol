ce2dfbaa16d0113b0d42c155df747d5f94875c49
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2018-05-23 16:00:20 +0530

    modified:   main.sol

--- main.sol
+++ main.sol
@@ -1,2 +1,27 @@
 pragma solidity ^0.4.0;
 
+contract Showman {
+
+  mapping (address => string) public users;
+  mapping (address => string[]) public posts;
+
+
+
+
+
+  function updateUser(string _name) public {
+
+    users[msg.sender] = _name;
+  }
+
+  function newPost(string _post) public {
+
+    posts[msg.sender][posts[msg.sender].length++] = _post;
+  }  
+
+  function totalPosts(address _add) public view returns (uint) {
+  	return posts[_add].length;
+  }
+
+
+}
