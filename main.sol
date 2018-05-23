4b0102645ead642bafc9d95a706c95c61f58b539
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2018-05-23 17:14:52 +0530

    modified:   main.sol

--- main.sol
+++ main.sol
@@ -9,12 +9,13 @@ contract Showman {
   struct User {
     string name;
     string aboutMe;
-  }    
+    string imageHash;
+  }
 
   struct Post {
     string post;
     uint time;
-  }    
+  }
 
   function updateUserName(string _name) public {
 
@@ -26,14 +27,18 @@ contract Showman {
     users[msg.sender].aboutMe = _aboutMe;
   }
 
+  function updateUserImageHash(string _imageHash) public {
+
+    users[msg.sender].imageHash = _imageHash;
+  }
+
   function newPost(string _post) public {
 
     uint p = posts[msg.sender].length++;
 
     posts[msg.sender][p].post = _post;
     posts[msg.sender][p].time = now;
-
-  }  
+  }
 
   function totalPosts(address _add) public view returns (uint) {
   	return posts[_add].length;
