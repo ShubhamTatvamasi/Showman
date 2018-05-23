eda77ee27fd12f39644e0ddcc0b3e6c7785091ea
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2018-05-23 16:24:42 +0530

    modified:   main.sol

--- main.sol
+++ main.sol
@@ -3,10 +3,12 @@ pragma solidity ^0.4.0;
 contract Showman {
 
   mapping (address => string) public users;
-  mapping (address => string[]) public posts;
+  mapping (address => Post[]) public posts;
 
-
-
+  struct Post {
+    string post;
+    uint time;
+  }    
 
 
   function updateUser(string _name) public {
@@ -16,7 +18,11 @@ contract Showman {
 
   function newPost(string _post) public {
 
-    posts[msg.sender][posts[msg.sender].length++] = _post;
+    uint p = posts[msg.sender].length++;
+
+    posts[msg.sender][p].post = _post;
+    posts[msg.sender][p].time = now;
+
   }  
 
   function totalPosts(address _add) public view returns (uint) {
