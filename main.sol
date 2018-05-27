a2f4c534e8044e4caee67aac18a08f1f6f0c9fbd
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2018-05-27 08:51:53 +0530

    modified:   main.sol

--- main.sol
+++ main.sol
@@ -1,24 +1,30 @@
 pragma solidity ^0.4.0;
 
 /** 
-* @title Project SHowman 
+* @title Project Showman
 * @author Shubham Tatvamasi
 * @notice Social Media Project for connecting to people
 */
 contract Showman {
 
+  /// @dev address of all the users
   mapping (address => User) public users;
+  /// @dev usernames of all the users
   mapping (string => address) usernames;
+  /// @dev list of posts of the user address
   mapping (address => uint[]) public postNumbers;
-
+  /// @dev to check if I am following the person or not
   mapping (address => mapping (address => Following)) public isFollowing;
-
+  /// @dev list of my followings
   mapping (address => address[]) public following;
+  /// @dev list of my followers
   mapping (address => address[]) public followers;
-
+  /// @dev list of total posts
   Post[] public posts;
+  /// @dev list of total feedbacks
   Feedback[] public feedbacks;
 
+  /// @dev structure for users
   struct User {
     string name;
     string username;
@@ -26,24 +32,28 @@ contract Showman {
     string imageHash;
   }
 
+  /// @dev structure for posts
   struct Post {
     string post;
     address from;
     uint time;
   }
 
+  /// @dev structure for following and followers users
   struct Following {
     bool status;
     uint followingPosition;
     uint followersPosition;
   }
 
+  /// @dev structure for feedbacks
   struct Feedback {
     string feedback;
     address from;
     uint time;
   }
 
+  /// @param _username for updating username
   function updateUsername(string _username) public {
     require(usernames[_username] == 0x0000000000000000000000000000000000000000);
 
@@ -52,27 +62,28 @@ contract Showman {
     usernames[_username] = msg.sender;
   }
 
+  /// @param _name for updating name
   function updateName(string _name) public {
-
     users[msg.sender].name = _name;
   }
 
+  /// @param _aboutMe for updating about me
   function updateAboutMe(string _aboutMe) public {
-
     users[msg.sender].aboutMe = _aboutMe;
   }
 
+  /// @param _imageHash for updating image
   function updateImageHash(string _imageHash) public {
-
     users[msg.sender].imageHash = _imageHash;
   }
 
+  /// @param _post for adding new post
   function newPost(string _post) public {
-
     postNumbers[msg.sender].push(totalPosts());
     posts.push(Post(_post, msg.sender, now));
   }
 
+  /// @param _follow new user
   function follow(address _follow) public {
     require(isFollowing[msg.sender][_follow].status == false);
     require(msg.sender != _follow);
@@ -83,6 +94,7 @@ contract Showman {
     followers[_follow].push(msg.sender);
   }
 
+  /// @param _unFollow the user which I am following
   function unFollow(address _unFollow) public {
     require(isFollowing[msg.sender][_unFollow].status == true);
 
@@ -101,48 +113,47 @@ contract Showman {
     isFollowing[msg.sender][_unFollow] = Following(false, 0, 0);
   }
 
+  /// @param _feedback add new feedback
   function newFeedback(string _feedback) public {
-    // add new feedback it array list
     feedbacks.push(Feedback(_feedback, msg.sender, now));
   }
 
+  /// @param _username to see the address
+  /// @return address of the _username
   function getUsernameAddress(string _username) public view returns (address) {
-    // return the address of the _username
     return usernames[_username];
   }
 
+  /// @return posts
   function totalPosts() public view returns (uint) {
-    // return the length of total number of posts
     return posts.length;
   }
 
+  /// @param _address to see total following
+  /// @return total posts of _address
   function totalPostNumbers(address _address) public view returns (uint) {
-    // return the length of total number of posts of _address
     return postNumbers[_address].length;
   }
 
+  /// @param _address to see total following
+  /// @return total following of _address
   function totalFollowing(address _address) public view returns (uint) {
-    // return the length of total number of following of _address
     return following[_address].length;
   }
 
-  /** 
-  * @dev Calculates the length of array
-  * @param _address Width of the rectangle
-  * @return length of total number of followers of _address
-  */
+  /// @param _address to see total followers
+  /// @return total followers of _address
   function totalFollowers(address _address) public view returns (uint) {
-    // return the length of total number of followers of _address
     return followers[_address].length;
   }
-
+   
+  /// @return total feedbacks
   function totalFeedbacks() public view returns (uint) {
-    // return the length of total number of feedbacks
     return feedbacks.length;
   }
 
+  /// @dev if ether is sent to this address, send it back
   function () public {
-    // if ether is sent to this address, send it back.
     revert();
   }
 
